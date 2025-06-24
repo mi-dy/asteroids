006a4d7d4efe6626f69087f600077f9186f3e5dd
mi-dy <mindaugasdy@gmail.com>
2025-06-24 18:36:11 +0300

    Added forward/backward movement for player model

--- main.py
+++ main.py
@@ -17,10 +17,6 @@ def main():
         player.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-
+        
 if __name__ == "__main__":
     main()
