0e0fda2e918deac2869aabb2d54380b67150602a
mi-dy <mindaugasdy@gmail.com>
2025-06-24 18:56:47 +0300

    Initialized object grouping

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ def main():
     dt = 0
     player = Player(x = SCREEN_WIDTH/2, y = SCREEN_HEIGHT/2)
     screen = pygame.display.set_mode(size=(SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+    Player.containers = (updatable, drawable)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, color="black")
-        player.draw(screen)
-        player.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        updatable.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
